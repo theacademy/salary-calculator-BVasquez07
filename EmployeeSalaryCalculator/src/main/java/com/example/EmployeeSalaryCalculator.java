cc42b9be37b9fddbf4433bd29fb14c8a0685195c
BVasquez07 <bbvvleonardwolf@gmail.com>
2026-02-19 23:21:29 -0500

    Commit2

--- EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
+++ EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
@@ -76,7 +76,7 @@ public class EmployeeSalaryCalculator
 					overTimeRate = 2 * hourlyRate;
 					overTimePay = overTimeRate * overTime;
 
-					basicPay = hourlyRate * hoursWorked;
+					basicPay = hourlyRate * 40; //capped at 40
 					grossSalary = basicPay + overTimePay;
 					//YOUR CODE ENDS HERE
 				}
